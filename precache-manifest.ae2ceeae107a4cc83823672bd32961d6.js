489f02bc56f5f3d5b9ff40c854efdeda9ef5fb17
pbutin <pierrick.butin.v2@gmail.com>
2020-10-13 14:47:49 +0200

    Updates

---
+++ precache-manifest.ae2ceeae107a4cc83823672bd32961d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0927fac26857f7e2f1b73c2a28119515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292f18a38510a7149512",
+    "url": "/static/css/main.f3cc8fdf.chunk.css"
+  },
+  {
+    "revision": "c680408bc1b5a6e8f155",
+    "url": "/static/js/2.c4e51d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4e51d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292f18a38510a7149512",
+    "url": "/static/js/main.0970dbe6.chunk.js"
+  },
+  {
+    "revision": "2ff867f720f5492aa46d",
+    "url": "/static/js/runtime-main.b9df5378.js"
+  }
+]);
